146abc33bd4aa728002a3966d38eb195ae4408e6
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-03-06 06:00:23 -0400

    Created a Reported Cases page for the Inquirer

---
+++ database/db.php
@@ -0,0 +1,14 @@
+//=====[DEFINING THE MYSQL DATABASE VARIABLES]======================================================
+
+<?php
+	$servername='localhost';
+	$username='root';
+	$password='';
+	$dbname = "my_db";
+	$conn=mysqli_connect($servername,$username,$password,"$dbname");
+	  if(!$conn){
+		  die('Could not Connect MySql Server:' .mysql_error());
+		}
+?>
+
+
